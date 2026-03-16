7cbc5d550ca097f3c54c7d03d73d0aa4da17167b
cheahy18-rgb <chea.hy18@gmail.com>
2026-03-16 21:50:08 +0700

    Add files via upload

---
+++ mybot.js
@@ -0,0 +1,47 @@
+const { Telegraf, Markup } = require('telegraf');
+
+const bot = new Telegraf('8190289200:AAFm7I5PtzeWTx2XKS85awuSICV42FRGDC4');
+
+bot.start((ctx) => {
+  ctx.reply(
+    'ស្វាគមន៍! សូមជ្រើសរើសប្រភេទទំនិញ 👇',
+    Markup.keyboard([
+      ['អាវ', 'ខ្សែក្រវ៉ាត់'],
+      ['ស្បែកជើង', 'ទំនក់ទំនង']
+    ]).resize()
+  );
+});
+
+bot.hears('អាវ', async (ctx) => {
+  await ctx.replyWithMediaGroup([
+    {
+      type: 'photo',
+      media: { source: 'shirt/shirt1.jpg' },
+    },
+    {
+      type: 'photo',
+      media: { source: 'shirt/shirt2.jpg' },
+      caption: 'ម៉ូដែល: អាវវ៉ែនតា\nតំលៃ: 15,000រៀល'
+    }
+  ]);
+});
+
+bot.hears('ខ្សែក្រវ៉ាត់', async (ctx) => {
+  await ctx.replyWithMediaGroup([
+    {
+      type: 'photo',
+      media: { source: 'belt/belt1.jpg' },
+    },
+    {
+      type: 'photo',
+      media: { source: 'belt/belt2.jpg' },
+      caption: 'ម៉ូដែល: ខ្សែក្រវ៉ាត់ស្បែក\nតំលៃ: 25,000រៀល'
+    }
+  ]);
+});
+
+bot.launch();
+console.log('Bot is running...');
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
